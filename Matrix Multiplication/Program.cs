49e10cc02ef30fb4ceb670c212603e228fa4a890
Danil <glazdan17@gmail.com>
2022-01-15 19:14:54 +0300

    NEWLife

--- Matrix Multiplication/Program.cs
+++ Matrix Multiplication/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Threading;
+using System.Threading.Tasks;
 
 namespace Matrix_Multiplication
 {
@@ -13,15 +13,11 @@ namespace Matrix_Multiplication
         public static int[,] matrix1;
         public static int[,] matrix2;
         public static int[,] MainMatrix;
-        private static Semaphore _semaphore = new Semaphore( 2,4); 
-        private static bool _flag = false;
         private static Random _random = new Random(); //для заполнения матриц
         
         public static void DisplayMatrix() //метод заполнения матриц
         {
-            Console.Write("Введите размер  квадратной матрицы. N= ");
-            
-
+            Console.Write("Введите размер матрицы. N= ");
             matrix1 = new int[N, N];
             Console.WriteLine("Матрица 1:");
             for (int i = 0; i < matrix1.GetLength(0); i++)
@@ -53,64 +49,48 @@ namespace Matrix_Multiplication
 
         public static void Main(string[] args)
         {
-            
             DisplayMatrix();
-            MainMatrix = new int[N, N];
-            
-
-            Thread[] calculators = new Thread[N * N];
-            for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
-                {
-                    calculators[N * i + j] = new Thread(Multiplication);
-                    calculators[N * i + j].Name = $"{i}{j}";
-                }
-            }
-
-            foreach (var c in calculators) //стартуем потоки
-            {
-                c.Start();
-                
-            }
-            
-
+            MainMatrix= new int[matrix1.GetLength(1), matrix2.GetLength(0)];
+            MainMatrix = Multiplication(matrix1, matrix2);
+            DisplayMainMatrix(MainMatrix);
             
         }
 
-        public static void Multiplication()
+        public static int[,] Multiplication(int[,] matrix1, int[,] matrix2)
         {
-
-            int m_matrix = new int();
-            int[] numbers = new int[2];
-            for (int i = 0; i < 2; i++)
+            int[,] result = new int[matrix1.GetLength(1),matrix2.GetLength(0)];
+            var timer = System.Diagnostics.Stopwatch.StartNew();
+            Parallel.For((long) 0, matrix1.GetLength(1), i =>
             {
-                numbers[i] = Int32.Parse(Thread.CurrentThread.Name[i].ToString());
-            }
-
-            for (int i = 0; i < N; i++)
-            {
-                m_matrix += matrix1[numbers[0], i] * matrix2[i, numbers[1]];
-            }
-
-            Console.WriteLine($"Поток {Thread.CurrentThread.Name} посчитал значение элемента матрицы {m_matrix}");
-            if (_flag) _semaphore.WaitOne();
-            
-
-            MainMatrix[numbers[0], numbers[1]]=m_matrix;
-                
-            Console.WriteLine($"Значение добавлено в матрицу на позицию [{numbers[0]}, {numbers[1]}]");
-            for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
+                for (int j = 0; j < matrix2.GetLength(0); j++)
                 {
-                    Console.Write($"{MainMatrix[i, j]}\t");
+                    result[i, j] = 0;
+                    for (int k = 0; k < matrix1.GetLength(1); k++)
+                    {
+                        result[i, j] += matrix1[i, k] * matrix2[k, j];
+                    }
                 }
+            });
 
+            timer.Stop();
+            Console.WriteLine($"Время вычислений в параллельном режиме {timer.ElapsedMilliseconds}");
+            return result;
+        }
+        static void DisplayMainMatrix(int[,] m)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    Console.Write(m[i, j]+" " );
+                }
+                
                 Console.WriteLine();
             }
         }
+       
         
+           
     }
 }
         
